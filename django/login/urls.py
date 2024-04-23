b03142693ac9ff7f71f1d0c18e0603438c03b8d9
Santiago Barragan <santiago@unlimitedrobotics.com>
2024-04-22 22:44:55 -0500

    feat(login): add contactanos page

--- django/login/urls.py
+++ django/login/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('contactanos', views.contactanos_page, name='contactanos')
 ]
--- django/login/views.py
+++ django/login/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'base.html')
+
+def contactanos_page(request):
+    return render(request, 'contactanos.html')
