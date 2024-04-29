0c8307c9c2d528651f0c38aaf1e9f2f6a9e400ec
Santiago Barragan <santiago@unlimitedrobotics.com>
2024-04-28 23:29:15 -0500

    [login] functional login

--- django/apps/login/urls.py
+++ django/apps/login/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('contactanos', views.contactanos_page, name='contactenos'),
-    path('login', views.login_form, name='login')
+    path('login', views.login_form, name='login'),
+    path('login_form', views.login_request, name='login_request')
 ]
--- django/apps/login/views.py
+++ django/apps/login/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def index(request):
@@ -9,3 +11,29 @@ def contactanos_page(request):
 
 def login_form(request):
     return render(request, 'login.html')
+
+
+def login_request(request):
+    data = dict()
+    data['msg'] = "success"
+    
+    try:
+        usuario = request.POST['usuario']
+        password = request.POST['password']
+        if password == '':
+            raise Exception("Contraseña vacia")
+        elif usuario == '':
+            raise Exception("Usuario vacio")
+        
+        user = authenticate(username=usuario, password=password)
+        if user is None:
+            raise Exception("Usuario no existe o contraseña incorrecta")
+        if user.is_active == False:
+            raise Exception("Usuario inactivo")
+        login(request, user)
+        data['url'] = '/dashboard'
+        return JsonResponse(data)
+    except Exception as e:
+        data['msg'] = str(e)
+    return JsonResponse(data)
+
