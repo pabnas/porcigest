390646b3df96d9295289a73f57d2d78c6cd54cd6
Alexander <alexander.bermudez@truehome.com.mx>
2024-05-07 23:33:46 -0500

    :fire: Side_bar

--- django/apps/dashboard/views.py
+++ django/apps/dashboard/views.py
@@ -4,4 +4,4 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 @login_required(login_url='/login', redirect_field_name=None)
 def index(request):
-    return render(request, 'dashboard.html')
+    return render(request, '_sidebar.html')
