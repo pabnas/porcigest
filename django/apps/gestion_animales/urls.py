acac58e64f37210f614be250cf61c52d4be7354b
Alexander <alexander.bermudez@truehome.com.mx>
2024-05-18 23:54:30 -0500

    :sparkles: Nuevos resumenes de tablas

--- django/apps/gestion_animales/urls.py
+++ django/apps/gestion_animales/urls.py
@@ -9,26 +9,31 @@ urlpatterns = [
     path('gestacion/', views.gestacion_index, name='gestacion'),
     path('gestacion/resumen', views.gestacion_resumen, name='gestacion_resumen', kwargs={'navbar': 'gestacion_resumen'}),
     path('gestacion/registrar', views.gestacion_register, name='gestacion_registrar', kwargs={'navbar': 'gestacion_registrar'}),
+    path('gestacion/list_gestacion', views.list_gestacion, name='list_gestacion'),
 
     # inventario
     path('inventario/', views.inventario_index, name='inventario'),
     path('inventario/resumen', views.inventario_resumen, name='inventario_resumen', kwargs={'navbar': 'inventario_resumen'}),
     path('inventario/registrar', views.inventario_register, name='inventario_registrar', kwargs={'navbar': 'inventario_registrar'}),
+    path('inventario/list_inventario', views.list_inventario, name='list_inventario'),
 
     # lactancia
     path('lactancia/', views.lactancia_index, name='lactancia'),
     path('lactancia/resumen', views.lactancia_resumen, name='lactancia_resumen', kwargs={'navbar': 'lactancia_resumen'}),
     path('lactancia/registrar', views.lactancia_register, name='lactancia_registrar', kwargs={'navbar': 'lactancia_registrar'}),
+    path('lactancia/list_partos', views.list_partos, name='list_partos'),
 
     # movimientos
     path('movimientos/', views.movimientos_index, name='movimientos'),
     path('movimientos/resumen', views.movimientos_resumen, name='movimientos_resumen', kwargs={'navbar': 'movimientos_resumen'}),
     path('movimientos/registrar', views.movimientos_register, name='movimientos_registrar', kwargs={'navbar': 'movimientos_registrar'}),
+    path('movimientos/list_movimientos', views.list_movimientos, name='list_movimientos'),
 
     # precebos
     path('precebos/', views.precebos_index, name='precebos'),
     path('precebos/resumen', views.precebos_resumen, name='precebos_resumen', kwargs={'navbar': 'precebos_resumen'}),
     path('precebos/registrar', views.precebos_register, name='precebos_registrar', kwargs={'navbar': 'precebos_registrar'}),
+    path('precebos/list_lotes_lechones', views.list_lotes_lechones, name='list_lotes_lechones'),
 
     # ficha_cerdos
     path('ficha_cerdos/', views.ficha_cerdos_index, name='ficha_cerdos'),
--- django/apps/gestion_animales/views.py
+++ django/apps/gestion_animales/views.py
@@ -1,7 +1,13 @@
+import datetime
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
-from django.models import InventarioAnimales
+from django.http import JsonResponse
+from models import InventarioAnimales
+from models import RegistroInseminaciones
+from models import RegistroPartos
+from models import Movimientos
+from models import LotesLechones
 
 # index
 @login_required(login_url='/login', redirect_field_name=None)
@@ -13,26 +19,33 @@ def index(request):
 def gestacion_index(request):
     return redirect('gestion_animales:gestacion_resumen')
 
+@login_required(login_url='/login', redirect_field_name=None)
+def list_gestacion(_request):
+    gestacion_all_rows = RegistroInseminaciones.objects.values()
+    for row in gestacion_all_rows:
+        row['fecha_parto_estimada'] = row['fecha_inseminacion'] + datetime.timedelta(days=114)
+    gestacion = list(gestacion_all_rows)
+    data = {'gestacion': gestacion}
+    return JsonResponse(data)
 
 @login_required(login_url='/login', redirect_field_name=None)
 def gestacion_resumen(request, **kwargs):
     return render(request, 'gestion_animales/gestacion/resumen.html')
-
-def list_gestacion(_request):
-    gestacion = list(InventarioAnimales.objects.values())
-    data = {'gestacion': gestacion}
-    return JsonResponse(data)
 
 @login_required(login_url='/login', redirect_field_name=None)
 def gestacion_register(request, **kwargs):
     return render(request, 'gestion_animales/gestacion/register.html')
 
 # inventario
-
 @login_required(login_url='/login', redirect_field_name=None)
 def inventario_index(request):
     return redirect('gestion_animales:inventario_resumen')
 
+@login_required(login_url='/login', redirect_field_name=None)
+def list_inventario(_request):
+    inventario = list(InventarioAnimales.objects.values())
+    data = {'inventario': inventario}
+    return JsonResponse(data)
 
 @login_required(login_url='/login', redirect_field_name=None)
 def inventario_resumen(request, **kwargs):
@@ -44,11 +57,15 @@ def inventario_register(request, **kwargs):
     return render(request, 'gestion_animales/inventario/register.html')
 
 # lactancia
-
 @login_required(login_url='/login', redirect_field_name=None)
 def lactancia_index(request):
     return redirect('gestion_animales:lactancia_resumen')
 
+@login_required(login_url='/login', redirect_field_name=None)
+def list_partos(_request):
+    partos = list(RegistroPartos.objects.values())
+    data = {'partos': partos}
+    return JsonResponse(data)
 
 @login_required(login_url='/login', redirect_field_name=None)
 def lactancia_resumen(request, **kwargs):
@@ -60,11 +77,15 @@ def lactancia_register(request, **kwargs):
     return render(request, 'gestion_animales/lactancia/register.html')
 
 # movimientos
-
 @login_required(login_url='/login', redirect_field_name=None)
 def movimientos_index(request):
     return redirect('gestion_animales:movimientos_resumen')
 
+@login_required(login_url='/login', redirect_field_name=None)
+def list_movimientos(_request):
+    movimientos = list(Movimientos.objects.values())
+    data = {'movimientos': movimientos}
+    return JsonResponse(data)
 
 @login_required(login_url='/login', redirect_field_name=None)
 def movimientos_resumen(request, **kwargs):
@@ -76,11 +97,15 @@ def movimientos_register(request, **kwargs):
     return render(request, 'gestion_animales/movimientos/register.html')
 
 # precebos
-
 @login_required(login_url='/login', redirect_field_name=None)
 def precebos_index(request):
     return redirect('gestion_animales:precebos_resumen')
 
+@login_required(login_url='/login', redirect_field_name=None)
+def list_lotes_lechones(_request):
+    lotes_lechones = list(LotesLechones.objects.values())
+    data = {'lotes_lechones': lotes_lechones}
+    return JsonResponse(data)
 
 @login_required(login_url='/login', redirect_field_name=None)
 def precebos_resumen(request, **kwargs):
@@ -92,7 +117,6 @@ def precebos_register(request, **kwargs):
     return render(request, 'gestion_animales/precebos/register.html')
 
 # ficha_cerdos
-
 @login_required(login_url='/login', redirect_field_name=None)
 def ficha_cerdos_index(request):
     return redirect('gestion_animales:ficha_cerdos_resumen')
