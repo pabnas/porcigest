fb5be4b68b18c2d902568d90605a5b39d31f66ed
Santiago Barragan <santiago@unlimitedrobotics.com>
2024-05-20 23:12:42 -0500

    :ambulance: fix labels with one word

--- django/apps/utils.py
+++ django/apps/utils.py
@@ -3,5 +3,5 @@ def snake_case_to_title(snake_case_string, decorator='* '):
     # Split the snake case string into words
     words = snake_case_string.split('_')
     # Capitalize the first word and lowercase the rest, then join them with a space
-    title_case_string = ' '.join(words[0].capitalize() + ' ' + word for word in words[1:])
+    title_case_string = ' '.join(words[0].capitalize() if i == 0 else word for i, word in enumerate(words))
     return title_case_string + decorator
