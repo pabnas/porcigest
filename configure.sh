0417d34753dd8e329b1d0a69a63aa5b9140f97ae
Santiago Barragan <santiago@unlimitedrobotics.com>
2024-05-20 23:13:08 -0500

    change to just one env file

--- configure.sh
+++ configure.sh
@@ -3,4 +3,4 @@
 # Script to run configuration with a bash terminal
 
 # Build the Docker image and start the container
-docker-compose --env-file ./.env up --build
+docker-compose --env-file ./django/porcigest/.env up --build
