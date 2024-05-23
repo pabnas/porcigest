c8c3f0a45854b5db1578f0e8b899ce9c0fcbb6c2
Santiago Barragan <santiago@unlimitedrobotics.com>
2024-05-22 20:02:08 -0500

    refactor scripts

--- configure.sh
+++ configure.sh
@@ -5,11 +5,7 @@ docker-compose build db
 if [[ "$1" == "-rpi" ]]; then
     echo "Building for Raspberry Pi."
     docker-compose build django-rpi
-    echo "Running for Raspberry Pi environment."
-    docker-compose up db django-rpi
 else
     echo "Building for non-Raspberry Pi environment."
     docker-compose build django
-    echo "Running for non-Raspberry Pi environment."
-    docker-compose up db django
 fi
--- runserver.sh
+++ runserver.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# Script to execute the command `python manage.py runserver 0.0.0.0:8000`
-
-# Run the Django server
-docker-compose exec web python manage.py runserver 0.0.0.0:8000
-
+if [[ "$1" == "-rpi" ]]; then
+    echo "Running for Raspberry Pi environment."
+    docker-compose up db django-rpi
+else
+    echo "Running for non-Raspberry Pi environment."
+    docker-compose up db django
+fi
